a3a6e6c0926c80673f27ab4b7576ed99ff1ed6d5
folz <joachim.folz@dfki.de>
2019-05-06 13:46:06 +0200

    fix build-wheels

--- build-wheels.sh
+++ build-wheels.sh
@@ -8,17 +8,17 @@ rm -r /opt/python/cp34*
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r requirements.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/ --no-deps
+    "${PYBIN}/pip" wheel . -w wheelhouse/ --no-deps
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" -w /io/wheelhouse/
+    auditwheel repair "$whl" -w wheelhouse/
 done
 
 # Install packages and test
 #for PYBIN in /opt/python/*/bin/; do
-#    "${PYBIN}/pip" install -r /io/test-requirements.txt
-#    "${PYBIN}/pip" install turbojpeg --no-index -f /io/wheelhouse
-#    "${PYBIN}/nosetests" -w /io/test
+#    "${PYBIN}/pip" install -r test-requirements.txt
+#    "${PYBIN}/pip" install turbojpeg --no-index -f wheelhouse
+#    "${PYBIN}/nosetests" -w test
 #done
