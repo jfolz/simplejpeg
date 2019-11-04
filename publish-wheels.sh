e1c6001909ba5812e3840f61a42f56a291ba27ce
folz <joachim.folz@dfki.de>
2019-11-04 16:25:00 +0100

    twine: surround u/p with quotes

--- publish-wheels.sh
+++ publish-wheels.sh
@@ -3,4 +3,4 @@ set -e -x
 
 PYBIN=/opt/python/cp37-cp37m/bin
 "${PYBIN}/pip" install twine
-"${PYBIN}/python" -m twine upload --skip-existing --disable-progress-bar -u ${TWINE_USERNAME} -p ${TWINE_PASSWORD} dist/*
+"${PYBIN}/python" -m twine upload --skip-existing --disable-progress-bar -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" dist/*
