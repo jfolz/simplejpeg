8b81c24d5d7ffc9e579eaae4a12d9888761995a2
Joachim Folz <joachim.folz@dfki.de>
2020-01-05 20:24:29 +0100

    install test requirements

--- build-wheels.ps1
+++ build-wheels.ps1
@@ -5,6 +5,7 @@ foreach ($python in $interpreters){
     & $python\python.exe -m pip install -U pip wheel
     & $python\python.exe -m pip install -r build-requirements.txt
     & $python\python.exe -m pip wheel . -w dist --no-deps
+    & $python\python.exe -m pip install -r test-requirements.txt
     & $python\python.exe -m pip install .
     cd test
     & $python\python.exe -m pytest -vv
--- build-wheels.sh
+++ build-wheels.sh
@@ -10,6 +10,7 @@ for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r build-requirements.txt
     "${PYBIN}/pip" wheel . -w wheelhouse/ --no-deps
     "${PYBIN}/pip" install .
+    "${PYBIN}/pip" install -r test-requirements.txt
     cd test
     "${PYBIN}/python" -m pytest -vv
     cd ..
