f2fec59fc839cbde602f38f967a2921330d91487
Joachim Folz <joachim.folz@dfki.de>
2020-01-05 16:12:15 +0100

    create sdist after appveyor import

--- build-wheels.sh
+++ build-wheels.sh
@@ -8,7 +8,6 @@ rm -r /opt/python/cp34*
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r build-requirements.txt
-    "${PYBIN}/python" setup.py sdist --dist-dir=dist
     "${PYBIN}/pip" wheel . -w wheelhouse/ --no-deps
 done
 
