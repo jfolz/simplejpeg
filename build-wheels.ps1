668b94b004d6f66e151325c3de432e63303e681d
Joachim Folz <joachim.folz@dfki.de>
2020-01-05 15:49:45 +0100

    build-wheels.ps1: fix python calls

--- build-wheels.ps1
+++ build-wheels.ps1
@@ -2,7 +2,7 @@ $interpreters = $env:INTERPRETERS -split ";"
 
 # Compile wheels
 foreach ($python in $interpreters){
-    $python\python.exe -m pip install -U pip wheel
-    $python\python.exe -m pip install -r build-requirements.txt
-    $python\python.exe -m pip wheel . -w wheelhouse --no-deps
+    & $python\python.exe -m pip install -U pip wheel
+    & $python\python.exe -m pip install -r build-requirements.txt
+    & $python\python.exe -m pip wheel . -w dist --no-deps
 }
