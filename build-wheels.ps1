214a0af4700d66041caaf1c2cdaae68473a54452
Joachim Folz <joachim.folz@dfki.de>
2020-01-15 19:03:52 +0100

    use $LASTEXITCODE to detect exceptions

--- build-wheels.ps1
+++ build-wheels.ps1
@@ -8,8 +8,8 @@ $interpreters = $env:INTERPRETERS -split ";"
 foreach ($python in $interpreters){
     & $python\python.exe -m pip install -U pip wheel --no-warn-script-location
     & $python\python.exe -m pip install -r build-requirements.txt --no-warn-script-location
-    #& $python\python.exe -m pip wheel . -v -w dist --no-deps
     & $python\python.exe setup.py bdist_wheel --dist-dir=dist
+    if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
 }
 
 # Install and test
@@ -18,5 +18,6 @@ foreach ($python in $interpreters){
     & $python\python.exe -m pip install -r ..\test-requirements.txt --no-warn-script-location
     & $python\python.exe -m pip install simplejpeg --no-index -f ..\dist
     & $python\python.exe -m pytest -vv
+    if ($LASTEXITCODE -ne 0) { throw "test failed with exit code $LASTEXITCODE" }
 }
 cd ..
