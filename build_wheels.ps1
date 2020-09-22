c914bf9316740343b6639ed4186f9a89e471260b
Joachim Folz <joachim.folz@gmail.com>
2020-09-22 21:37:28 +0000

    set ErrorActionPreference to Continue

--- build_wheels.ps1
+++ build_wheels.ps1
@@ -1,5 +1,5 @@
-# stop on errors
-$ErrorActionPreference = "Stop";
+# do not stop on errors, manually catch errors based on exit code
+$ErrorActionPreference = "Continue";
 
 # the list of Python interpreters
 $interpreters = $env:INTERPRETERS -split ";"
@@ -7,7 +7,9 @@ $interpreters = $env:INTERPRETERS -split ";"
 # Compile wheels
 foreach ($python in $interpreters){
     & $python\python.exe -m pip install -U pip --no-warn-script-location
+    if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
     & $python\python.exe -m pip install -r build_requirements.txt --no-warn-script-location
+    if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
     & $python\python.exe setup.py bdist_wheel --dist-dir=dist
     if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
 }
@@ -16,7 +18,9 @@ foreach ($python in $interpreters){
 cd test
 foreach ($python in $interpreters){
     & $python\python.exe -m pip install -r ..\test_requirements.txt --no-warn-script-location
+    if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
     & $python\python.exe -m pip install simplejpeg --no-index -f ..\dist
+    if ($LASTEXITCODE -ne 0) { throw "build failed with exit code $LASTEXITCODE" }
     & $python\python.exe -m pytest -vv
     if ($LASTEXITCODE -ne 0) { throw "test failed with exit code $LASTEXITCODE" }
 }
