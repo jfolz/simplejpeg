2c8454bb3036c0106403624066565b2aaf86eb19
Joachim Folz <joachim.folz@gmail.com>
2020-11-09 22:25:21 +0000

    Py39

--- check_artifacts.sh
+++ check_artifacts.sh
@@ -4,5 +4,5 @@ set -e -x
 PYBIN=/opt/python/cp38-cp38/bin
 "${PYBIN}/python" check_artifacts.py \
     dist \
-    --python-versions cp35 cp36 cp37 cp38 \
+    --python-versions cp35 cp36 cp37 cp38 cp39 \
     --platforms manylinux2010_x86_64 manylinux2010_i686 win_amd64 win32 macosx_10_9_x86_64
