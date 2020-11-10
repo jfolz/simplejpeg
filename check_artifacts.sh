5847b0c86d17a96226c9051c8ef6e2a4413ac72b
Joachim Folz <joachim.folz@gmail.com>
2020-11-10 09:51:14 +0000

    check_artifacts: check for manylinux1 wheels

--- check_artifacts.sh
+++ check_artifacts.sh
@@ -5,4 +5,7 @@ PYBIN=/opt/python/cp38-cp38/bin
 "${PYBIN}/python" check_artifacts.py \
     dist \
     --python-versions cp35 cp36 cp37 cp38 cp39 \
-    --platforms manylinux2010_x86_64 manylinux2010_i686 win_amd64 win32 macosx_10_9_x86_64
+    --platforms manylinux1_x86_64 manylinux1_i686 \
+                manylinux2010_x86_64 manylinux2010_i686 \
+                win_amd64 win32 \
+                macosx_10_9_x86_64
