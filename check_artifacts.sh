7df45d616e645bae988df97963b345484fb070b2
Joachim Folz <joachim.folz@gmail.com>
2021-04-27 12:14:04 +0000

    use manylinux_2_24 instead of manylinux2014

--- check_artifacts.sh
+++ check_artifacts.sh
@@ -2,14 +2,18 @@
 set -e -x
 
 PYBIN=/opt/python/cp39-cp39/bin
-"${PYBIN}/python" check_artifacts.py \
-    dist \
+"${PYBIN}/python" check_artifacts.py dist \
     --python-versions cp36 cp37 cp38 cp39 \
-    --platforms manylinux1_x86_64 \
-                manylinux1_i686 \
-                manylinux2010_x86_64 \
+    --platforms manylinux1_i686 \
+                manylinux1_x86_64 \
                 manylinux2010_i686 \
-                manylinux2014_x86_64 \
+                manylinux2010_x86_64 \
                 manylinux2014_i686 \
-                win_amd64 win32 \
+                manylinux2014_x86_64 \
+                manylinux2014_aarch64 \
+                manylinux_2_24_i686 \
+                manylinux_2_24_x86_64 \
+                manylinux_2_24_aarch64 \
+                win_amd64 \
+                win32 \
                 macosx_10_9_x86_64
