2cc564b3bb42213830482367a65fafb6a5127931
Joachim Folz <joachim.folz@gmail.com>
2021-05-28 23:58:40 +0000

    macos: python -m pip

--- build_wheels_macos.sh
+++ build_wheels_macos.sh
@@ -9,15 +9,15 @@ eval "$(pyenv init -)"
 for PYVER in ${PYVERS}; do
     pyenv install "${PYVER}"
     pyenv shell "${PYVER}"
-    pip install -U pip
-    pip install --only-binary ":all:" -r build_requirements.txt
-    pip wheel . -v -w wheelhouse/ --no-deps --use-feature=in-tree-build
-    pip uninstall -y numpy oldest-supported-numpy
+    python -m pip install -U pip
+    python -m pip install --only-binary ":all:" -r build_requirements.txt
+    python -m pip wheel . -v -w wheelhouse/ --no-deps --use-feature=in-tree-build
+    python -m pip uninstall -y numpy oldest-supported-numpy
 done
 
 # Bundle external shared libraries into the wheels
-pyenv global 3.8-dev
-pip install delocate
+pyenv shell 3.8-dev
+python -m pip install delocate
 for whl in wheelhouse/*.whl; do
     delocate-wheel -w dist -v "${whl}"
 done
@@ -25,9 +25,9 @@ done
 # Install and test
 cd test
 for PYVER in ${PYVERS}; do
-    pyenv global "${PYVER}"
-    pip install --only-binary ":all:" -r ../test_requirements.txt
-    pip install simplejpeg --no-index -f ../dist
+    pyenv shell "${PYVER}"
+    python -m pip install --only-binary ":all:" -r ../test_requirements.txt
+    python -m pip install simplejpeg --no-index -f ../dist
     python -m pytest -vv
 done
 cd ..
